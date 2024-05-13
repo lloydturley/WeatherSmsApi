44ae14359237002448d7ccc2c8cd9716de929b22
Lloyd Turley <Lloyd.Turley@hyland.com>
2024-05-13 19:33:52 -0400

    version 1 with forecast collection

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ parameters = {
     "appid": "d66a5de821dbf9153c40a062b1c9f700",
     #"lat": 35.121929,
     #"lon": -85.344170,
-    "lat":33.340179,
-    "lon":-86.630157,
-    "cnt":4,
+    "lat": 33.340179,
+    "lon": -86.630157,
+    "cnt": 4,
 }
 
 response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
@@ -14,9 +14,7 @@ response.raise_for_status()
 forecast = response.json()["list"]
 
 for fore in forecast:
-    if fore["weather"][0]["id"] < 701:
+    if int(fore["weather"][0]["id"]) < 701:
         # print(fore["weather"]["description"])
         print("Bring an umbrella")
         break
-
-
