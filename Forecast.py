a8fa4e7ead383dcd2b2535750f18218834cb93b2
Lloyd Turley <Lloyd.Turley@hyland.com>
2024-05-14 15:41:17 -0400

    added forecast collection into a list of forecasts

---
+++ Forecast.py
@@ -0,0 +1,18 @@
+import time
+
+from DateTime import DateTime
+
+# create a class that will hold a forecast with the fields date & time, feels_like, humidity, clouds, wind, rain, and weather_description
+
+class Forecast:
+    def __init__(self, date_time, feels_like, humidity, clouds, wind, rain, weather_description):
+        self.date_time = date_time
+        self.feels_like = feels_like
+        self.humidity = humidity
+        self.clouds = clouds
+        self.wind = wind
+        self.rain = rain
+        self.weather_description = weather_description
+
+
+
