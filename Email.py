5e593be1f8a42284111e3bcc1efc3198ff8ce4b4
Lloyd Turley <Lloyd.Turley@hyland.com>
2024-05-15 22:02:55 -0400

    completed the email.  V1 Done!

--- Email.py
+++ Email.py
@@ -4,15 +4,15 @@ import smtplib
 class Email:
 
     def __init__(self):
-        self.body = ""
+        self.body = "Here is your daily report\r\n"
 
     def send_email(self, login, password, subject, to_addr):
+        print(self.body)
         with smtplib.SMTP("smtp-mail.outlook.com", port=587) as connection:
             connection.starttls()
             connection.login(user=login, password=password)
             connection.sendmail(from_addr=login, to_addrs=to_addr,
-                                msg=f"Subject:{subject}\n\n{self.body}\n")
+                                msg=f"Subject:{subject}\n\n{self.body.encode("ascii", errors="ignore").decode()}")
 
     def add_to_body(self, message=""):
-        self.body += message + "\n"
-        print(self.body)
+        self.body += f"{message}\r\n"
--- StockMonitor.py
+++ StockMonitor.py
@@ -7,7 +7,6 @@ class StockMonitor:
 
     def __init__(self, tickers):
         self.tickers = tickers
-        print(self.tickers)
         self.stocks = []
 
 
@@ -20,41 +19,28 @@ class StockMonitor:
                 "datatype": "json",
                 "apikey": stockApiKey,
             }
-            # url = '?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
             #response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=full&apikey=demo")
             response = requests.get("https://www.alphavantage.co/query", params=parameters)
             response.raise_for_status()
             data = response.json()
-            print(data)
-            print()
-
-            yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-            yesterday = yesterday.strftime('%Y-%m-%d')
-            print(f"yesterday: {yesterday}")
-            print()
-            day_before = datetime.datetime.now() - datetime.timedelta(days=2)
-            day_before = day_before.strftime('%Y-%m-%d')
-            thirty_days_ago = datetime.datetime.now() - datetime.timedelta(days=30)
-            thirty_days_ago = thirty_days_ago.strftime('%Y-%m-%d')
-            ninety_days_ago = datetime.datetime.now() - datetime.timedelta(days=90)
-            ninety_days_ago = ninety_days_ago.strftime('%Y-%m-%d')
+            closings = list(data["Time Series (Daily)"].values())
 
             stock = Stock(
                 ticker=ticker,
                 change=self.calc_change(
-                    last_close=float(data["Time Series (Daily)"][yesterday]["4. close"]),
-                    prev_close=float(data["Time Series (Daily)"][day_before]["4. close"]),
+                    last_close=float(closings[0]["4. close"]),
+                    prev_close=float(closings[1]["4. close"]),
                 ),
-                last_close=data["Time Series (Daily)"][yesterday]["4. close"],
-                thirty_day_close=data["Time Series (Daily)"][thirty_days_ago]["4. close"],
+                last_close=float(closings[0]["4. close"]),
+                thirty_day_close=float(closings[29]["4. close"]),
                 thirty_day_change=self.calc_change(
-                    last_close=float(data["Time Series (Daily)"][yesterday]["4. close"]),
-                    prev_close=float(data["Time Series (Daily)"][thirty_days_ago]["4. close"]),
+                    last_close=float(closings[0]["4. close"]),
+                    prev_close=float(closings[29]["4. close"])
                 ),
-                ninety_day_close=data["Time Series (Daily)"][ninety_days_ago]["4. close"],
+                ninety_day_close=float(closings[89]["4. close"]),
                 ninety_day_change=self.calc_change(
-                    last_close=float(data["Time Series (Daily)"][yesterday]["4. close"]),
-                    prev_close=float(data["Time Series (Daily)"][ninety_days_ago]["4. close"]),
+                    last_close=float(closings[0]["4. close"]),
+                    prev_close=float(closings[89]["4. close"])
                 )
             )
             self.stocks.append(stock)
